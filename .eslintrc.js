1dcf4901d91394deb05fd644396de7a09c622209
Bruno Soaress 🍔 <bruno@stoodi.com.br>
2018-06-05 18:24:42 -0300

    Fix call method fetch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
-    ]
+  extends: "airbnb",
+  plugins: ["react", "jsx-a11y", "import"],
+  env: {
+    browser: true
+  }
 };
