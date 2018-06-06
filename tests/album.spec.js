87b83010737be7170ac2c489b766f76f53b19dc8
Bruno Soares <bruno@stoodi.com.br>
2018-06-05 23:27:29 -0300

    Create getAlbum method and test

---
+++ tests/album.spec.js
@@ -0,0 +1,62 @@
+// getAlbum
+// getAlbumTracks
+
+import chai, { expect } from 'chai';
+
+import { getAlbum, getAlbumTracks } from '../src/album';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+sinonStubPromise(sinon);
+
+chai.use(sinonChai);
+global.fetch = require('node-fetch');
+
+describe('Album', () => {
+  let fetchedStub;
+  let promise;
+  beforeEach(() => {
+    fetchedStub = sinon.stub(global, 'fetch');
+    promise = fetchedStub.returnsPromise();
+  });
+
+  afterEach(() => {
+    fetchedStub.restore();
+  });
+
+  describe('smoke tests', () => {
+    it('should be exists album method', () => {
+      expect(getAlbum).to.exist;
+    });
+
+    it('should be exist getAlbumTracks method', () => {
+      expect(getAlbumTracks).to.exist;
+    });
+  });
+
+  describe('getAlbum', () => {
+    it('should call fetch function', () => {
+      const album = getAlbum();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('shoul call fetch method with the correct URL', () => {
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
+      expect(fetchedStub).to.have.been.calledWith(
+        'https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy'
+      );
+
+      const album2 = getAlbum('4aawyAB9vmqN3uQ7FjRGTk');
+      expect(fetchedStub).to.have.been.calledWith(
+        'https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTk'
+      );
+    });
+
+    it('should return the correct data from Promise', () => {
+      promise.resolves({ album: 'name' });
+      const album = getAlbum('4aawyAB9vmqN3uQ7FjRGTk');
+
+      expect(album.resolveValue).to.be.eql({ album: 'name' });
+    });
+  });
+});
