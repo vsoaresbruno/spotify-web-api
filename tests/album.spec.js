5900ae2e1305558b081efa5ed79f1df064965324
Bruno Soares <bruno@stoodi.com.br>
2018-06-05 23:31:29 -0300

    Change order function called

--- tests/album.spec.js
+++ tests/album.spec.js
@@ -1,6 +1,3 @@
-// getAlbum
-// getAlbumTracks
-
 import chai, { expect } from 'chai';
 
 import { getAlbum, getAlbumTracks } from '../src/album';
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,8 +2,9 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
-chai.use(sinonChai);
 sinonStubPromise(sinon);
+
+chai.use(sinonChai);
 
 global.fetch = require('node-fetch');
 
